1d164f3c94c3f92e929f9f0bf5ee72299b12b58a
ronger <ronger-x@outlook.com>
2025-02-11 10:38:33 +0800

    wifi handle

--- esp32s3/src/wifi/WIFIHandle.cpp
+++ esp32s3/src/wifi/WIFIHandle.cpp
@@ -3,3 +3,92 @@
 //
 
 #include "WIFIHandle.h"
+#include "WiFi.h"
+
+
+WIFIHandle::WIFIHandle() : server(80) {}
+
+void WIFIHandle::begin() {
+    if (!SPIFFS.begin(true)) {
+        Serial.println("Failed to mount SPIFFS");
+        return;
+    }
+    loadCredentials();
+}
+
+void WIFIHandle::connect() {
+    if (credentials.isNull() || credentials.size() == 0) {
+        startAccessPoint();
+        return;
+    }
+
+    for (JsonPair kv : credentials.as<JsonObject>()) {
+        String ssid = kv.key().c_str();
+        String password = kv.value().as<String>();
+        
+        for (int i = 0; i < MAX_RETRIES; i++) {
+            if (tryConnect(ssid, password)) {
+                Serial.println("Connected successfully!");
+                return;
+            }
+            delay(5000);
+        }
+    }
+    
+    startAccessPoint();
+}
+
+bool WIFIHandle::tryConnect(const String& ssid, const String& password) {
+    Serial.printf("Trying to connect to: %s\n", ssid.c_str());
+    WiFi.begin(ssid.c_str(), password.c_str());
+    
+    unsigned long start = millis();
+    while (millis() - start < 10000) {
+        if (WiFi.status() == WL_CONNECTED) {
+            return true;
+        }
+        delay(500);
+    }
+    return false;
+}
+
+void WIFIHandle::startAccessPoint() {
+    apMode = true;
+    String apSSID = "esp32-" + getMacAddress();
+    
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(apSSID.c_str());
+    
+    server.on("/", HTTP_GET, [this](){ servePortal(); });
+    server.on("/configure", HTTP_POST, [this](){ handleFormSubmit(); });
+    server.begin();
+    
+    Serial.println("AP Mode Started");
+    Serial.println("SSID: " + apSSID);
+    Serial.println("IP: " + WiFi.softAPIP().toString());
+}
+
+void WIFIHandle::servePortal() {
+    String html = R"(
+        <form action='/configure' method='post'>
+            SSID: <input type='text' name='ssid'><br>
+            Password: <input type='password' name='password'><br>
+            <input type='submit' value='Connect'>
+        </form>
+    )";
+    server.send(200, "text/html", html);
+}
+
+void WIFIHandle::handleFormSubmit() {
+    String ssid = server.arg("ssid");
+    String password = server.arg("password");
+    
+    if (ssid.length() > 0) {
+        saveCredentials(ssid, password);
+        server.send(200, "text/plain", "Credentials saved. Connecting...");
+        delay(1000);
+        if (tryConnect(ssid, password)) {
+            resetAndRestart();
+        }
+    }
+}
--- esp32s3/src/wifi/WIFIHandle.h
+++ esp32s3/src/wifi/WIFIHandle.h
@@ -5,12 +5,41 @@
 #ifndef WIFI_HANDLE_H
 #define WIFI_HANDLE_H
 
+#pragma once
+#include <WiFi.h>
+#include <WebServer.h>
+#include <SPIFFS.h>
+#include <ArduinoJson.h>
 
+#define MAX_RETRIES 3
+#define CREDENTIALS_FILE "/wifi_config.json"
 
 class WIFIHandle {
+public:
+    WIFIHandle();
+    void begin(); // 初始化Wi-Fi处理
+    void connect(); // 尝试连接到Wi-Fi
+    void startAccessPoint(); // 启动AP模式
+    void handleClient(); // 处理连接到AP的客户端
+    void saveCredentials(const String& ssid, const String& password); // 存储Wi-Fi凭据
 
+private:
+    struct WifiCredential {
+        String ssid;
+        String password;
+    };
+
+    WebServer server;
+    bool apMode = false;
+    JsonDocument credentials;
+
+    bool tryConnect(const String& ssid, const String& password); // 尝试连接指定的SSID
+    void loadCredentials(); // 从存储中加载SSID和password
+    void resetAndRestart(); // 重置设备并重启
+    String getMacAddress(); // 获取设备的MAC地址
+    void servePortal();
+    void handleFormSubmit();
 };
-
 
 
 #endif //WIFI_HANDLE_H
